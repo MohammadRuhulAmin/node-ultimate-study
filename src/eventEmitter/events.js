b6a5d70ef1ca9f91a9ec7e187cff85bfb009f043
Md. Ruhul Amin <ruhul@ba-systems.com>
2026-01-01 15:01:51 +0600

    Add dynamic event name

--- src/eventEmitter/events.js
+++ src/eventEmitter/events.js
@@ -1,24 +1,23 @@
 const EventEmitter = require("events")
+"use strict";
 
 class EventEmit extends EventEmitter{
-    #eventName = "trigger"
-    constructor(){
+    #eventName
+    constructor(eventName){
         super();
+        this.#eventName = eventName;
         this.#registerEvent();
     }
     #registerEvent(){
-        this.on(this.#eventName,this.#handlerFunction.bind(this))
+        this.on(this.#eventName, this.#handleEvent.bind(this))
     }
-    #handlerFunction(){
-        for(let i = 1;i<5;i++){
-            console.log(`processing..${i}`)
-        }
+    #handleEvent(){
+        console.log("process has been called..")
     }
     fireEvent(){
         this.emit(this.#eventName)
     }
-
 }
 
-const eon = new EventEmit()
-eon.fireEvent()
+const eon1 = new EventEmit("trigger-x")
+eon1.fireEvent()
