3c07c737e62f5e5dd20b2853dc7b9fc49fa7771c
Md. Ruhul Amin <ruhul@ba-systems.com>
2026-01-01 15:36:09 +0600

    Add variable parameter in event

--- src/eventEmitter/events.js
+++ src/eventEmitter/events.js
@@ -1,23 +1,27 @@
-const EventEmitter = require("events")
+const EventEmitter = require("node:events")
 "use strict";
 
 class EventEmit extends EventEmitter{
     #eventName
-    constructor(eventName){
+    #processType
+    #certificate
+    constructor(eventName, processType, certificate){
         super();
+        this.#processType = processType;
+        this.#certificate = certificate;
         this.#eventName = eventName;
         this.#registerEvent();
     }
     #registerEvent(){
-        this.on(this.#eventName, this.#handleEvent.bind(this))
+        this.on(this.#eventName, ()=>this.#handleEvent())
     }
     #handleEvent(){
-        console.log("process has been called..")
+        console.log(`Process Type: ${this.#processType} \nCertificate: ${this.#certificate}`)
     }
     fireEvent(){
         this.emit(this.#eventName)
     }
 }
 
-const eon1 = new EventEmit("trigger-x")
+const eon1 = new EventEmit("trigger-x", "my-x", "intern")
 eon1.fireEvent()
